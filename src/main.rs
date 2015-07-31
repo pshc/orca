9e41be4d43783674c2590fdd0af97ff17c91f05b
Paul Collier <paul@paulcollier.ca>
2015-07-31 16:20:01 -0700

    Basic measure-then-arrange layout passes

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,170 @@ extern crate image;
 #[macro_use]
 extern crate log;
 
+use std::cmp;
 use std::fs::File;
 use std::process::Command;
 
-use presses::Press;
+use presses::{Paper, Press};
 
 mod presses;
 
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+struct Size(u32, u32);
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+struct Pos(i32, i32);
+
+
+fn draw_math<I: Paper>(paper: &mut I) {
+    let ref press = presses::FreeTypePress::new().unwrap();
+
+    const N: usize = 5;
+    let strs = ["+", "4", "-", "2", "1"];
+    let tree = vec![Children(2), Children(0), Children(2), Children(0), Children(0)];
+    let tree = Tree::new(&tree[..]);
+
+    let mut c_size = [Size(0, 0); N];
+    compute_sizes(&tree, &mut c_size);
+
+    let mut c_pos = [Pos(0, 0); N];
+    compute_positions(&tree, &c_size, &mut c_pos);
+
+    for ix in 0..N {
+        let Pos(x, y) = c_pos[ix];
+        press.blit_str(strs[ix], (x, y), paper).unwrap();
+    }
+}
+
+/// Determines how many children a tree node has.
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct Children(u32);
+
+/// Represents a tree hierarchy.
+pub struct Tree<'a> {
+    pub tree: &'a [Children],
+}
+
+impl<'a> Tree<'a> {
+    pub fn new(tree: &'a [Children]) -> Self {
+        assert!(tree.len() > 0, "tree must have root");
+        Tree {
+            tree: tree,
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.tree.len()
+    }
+
+    /// Depth-first bottom-up pass over the tree.
+    ///
+    /// Values of type R originate in leaves, propagating to the root.
+    pub fn flow_up<F, R>(&self, mut f: F) -> R
+        where F: FnMut(usize, &[R]) -> R {
+
+        let (ix, r) = self.flow_up_subtree(&mut f, 0);
+        // check that we actually saw the whole tree
+        assert_eq!(ix, self.len());
+        r
+    }
+
+    /// So is there a carrot to use this?
+    /// Safe, easy parallelism? Or is this just for show?
+    fn flow_up_subtree<F, R>(&self, f: &mut F, root_ix: usize) -> (usize, R)
+        where F: FnMut(usize, &[R]) -> R {
+
+        // descend first to leaves
+        let Children(n) = self.tree[root_ix];
+        let mut child_ix = root_ix + 1;
+
+        let r = if n > 0 {
+            // gaaah, allocation.
+            let mut rets = Vec::with_capacity(n as usize);
+            for _ in 0..n {
+                // can these three be combined into one?
+                let (new_ix, r) = self.flow_up_subtree(f, child_ix);
+                child_ix = new_ix;
+                rets.push(r);
+            }
+            f(root_ix, &rets)
+        }
+        else {
+            f(root_ix, &[])
+        };
+
+        (child_ix, r)
+    }
+}
+
+fn compute_sizes(tree: &Tree, sizes: &mut [Size]) {
+    assert!(sizes.len() >= tree.len(), "Not enough Sizes allocated");
+
+    // measure everything, starting bottom-up
+    // currently nothing fancy like margins or padding
+    let _total_size = tree.flow_up(|ix, child_sizes| -> Size {
+
+        // compute this from content
+        let my_size = Size(10, 10);
+
+        // size is sum of widths and max of heights
+        let size = child_sizes.iter().fold(my_size, |total, child: &Size| {
+            let sum_w = child.0 + total.0;
+            let max_h = cmp::max(child.1, total.1);
+            Size(sum_w, max_h)
+        });
+        sizes[ix] = size;
+        size
+    });
+}
+
+fn compute_positions(tree: &Tree, sizes: &[Size], coords: &mut [Pos]) {
+    assert!(sizes.len() >= tree.len(), "Not enough Sizes allocated");
+    assert!(coords.len() >= tree.len(), "Not enough Pos' allocated");
+
+    let mut stack = vec![(Pos(0, 0), 1)];
+    for ix in 0..tree.len() {
+        // this is pretty awkward. maybe ditch last_mut?
+        let (pop, push) = match stack.last_mut().expect("unexpected end of tree") {
+            &mut (ref mut cursor, ref mut n_siblings) => {
+                coords[ix] = *cursor;
+
+                // if we have children, push the cursor and sibling count on the stack
+                let Children(n) = tree.tree[ix];
+                let push = if n > 0 {
+                    let mut child_pos = *cursor;
+                    // hack: move cursor past the content of this node
+                    child_pos.0 += 10;
+                    Some((child_pos, n))
+                }
+                else {
+                    None
+                };
+
+                // if we're done at this level, pop back out to our old cursor
+                let size = sizes[ix];
+                cursor.0 += size.0 as i32;
+                assert!(*n_siblings > 0);
+                *n_siblings -= 1;
+                let pop = *n_siblings == 0;
+
+                (pop, push)
+            }
+        };
+        if pop {
+            stack.pop().unwrap();
+        }
+        if let Some(state) = push {
+            stack.push(state);
+        }
+    }
+    assert_eq!(stack.len(), 0);
+}
+
 fn main() {
-    let press = presses::FreeTypePress::new().unwrap();
+    let mut img = image::ImageBuffer::new(100, 40);
 
-    let mut img = image::ImageBuffer::new(40, 20);
-
-    press.blit_str("Hello, world!", (0, 0), &mut img).unwrap();
+    draw_math(&mut img);
 
     let filename = "out.png";
     let ref mut fout = File::create(filename).unwrap();
